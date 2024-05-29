aae34b9be1331a27ab320fcafa46967780ca5c33
Vishal Gupta <170102988+alwaysvishal@users.noreply.github.com>
2024-05-29 23:00:39 +0530

    Question13.c

---
+++ Question13.c
@@ -0,0 +1,31 @@
+// Vishal Gupta
+// 2303510100108
+
+#include <stdio.h>
+
+int main()
+{
+    int num, sum = 0;
+
+    printf("Enter any positive number: ");
+    scanf("%d", &num);
+
+    for (int i = 1; i < num; i++)
+    {
+        if (num % i == 0)
+        {
+            sum += i;
+        }
+    }
+
+    if (sum == num)
+    {
+        printf("%d is a perfect number.\n", num);
+    }
+    else
+    {
+        printf("%d is not a perfect number.\n", num);
+    }
+
+    return 0;
+}
